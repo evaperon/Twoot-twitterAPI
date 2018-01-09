218d65d1eefdfb390d32f716145e5377b96a1955
Eva <evelescout@gmail.com>
2018-01-09 21:24:11 +0200

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -12,12 +12,17 @@ collections = ['amtrak','Trend2','Trend3','Trend4', 'Trend5']
 
 #stopWords =  stopwords.words('english')
 
+#this function returns the 50 most used words and the whole list of unique word
+#and the times they appear in the collection and the collection's word count
+
 def countUniqueWords(tweets):
     #print(len(tweets))
     uniqueWords = {}
+    wordCount = 0
     for tweet in tweets:            
         for word in tweet:
             #print (word)
+            wordCount += 1
             if word in uniqueWords:
                  uniqueWords[word] += 1
             else:
@@ -26,12 +31,13 @@ def countUniqueWords(tweets):
     #print(uniqueWords)
     sortedWords = sorted(uniqueWords.items(), key = lambda x : x[1])
     #print (sortedWords)
-    top50 = list(reversed(sortedWords))[:50]
+    sortedWords = list(reversed(sortedWords))
+    top50 = sortedWords[:50]
     #print(type(top50))
 	
     #for i,word in enumerate(top50):
         #print(i+1,word)
-    return top50
+    return top50, sortedWords, wordCount
 
 
 #for every trend
@@ -65,20 +71,31 @@ for collection in collections:
         #remove special characters, numbers and stopwords and normalize the remaining words
         tweets[i][:] = [word.lower() for word in tweets[i] if word.isalpha()]# and word not in stopWords] 
 
-    top50WordsWithoutStopwordRemoval = countUniqueWords(tweets)
+    top50WordsWithoutStopwordRemoval, words, wordC = countUniqueWords(tweets)
+    print (wordC)
+    words225 = words[:225]
+    allTheWords = [i[0] for i in words225]
+    allTheWordsC = [i[1] for i in words225]
+    
+    plt.figure(1)
+    plt.subplot(211)
+    plt.bar(range(len(allTheWords)),allTheWordsC, tick_label=allTheWords, align='center', color='red')
+    plt.xticks(rotation='vertical')
+    plt.ylabel('Word Count')
+    
     #print(top50WordsWithoutStopwordRemoval)
-    wordCounts = [int(i[1]) for i in top50WordsWithoutStopwordRemoval]
+    wordCounts = [i[1] for i in top50WordsWithoutStopwordRemoval]
     topWords = [i[0] for i in top50WordsWithoutStopwordRemoval]
     #print(wordCounts)
     #print(topWords)
+    plt.subplot(212)
     plt.bar(topWords,wordCounts,align='center')
+    plt.xticks(rotation='vertical')
     plt.ylabel('Word Counts')
-    plt.xlabel('Words')
+
+    
     #the following commented part is for lines to show for each bar
     #but it looks bad so I scrapped it
     #for i in range(len(wordCounts)):
         #plt.hlines(wordCounts[i],0,topWords[i])
     plt.show(block=False)
-        
-	
-
