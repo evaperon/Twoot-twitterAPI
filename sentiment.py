37c0991d77f3ea7f84ed2808aa31444c4119cdba
Christina Ovezik <christino@csd.auth.gr>
2018-01-19 20:49:04 +0000

    Needs some pies

--- sentiment.py
+++ sentiment.py
@@ -9,28 +9,37 @@ db = client.twootdb
 
 collections, collectionsWithStopwords, collectionsWithoutStopwords, tweetsIds = parseTweets()
 
-tweetsWithLabels = {}
-count = 0
-for i,collection in enumerate(collectionsWithoutStopwords):
-    for j,tweet in enumerate(collection):
-        try:
-            strTweet = ' '.join(tweet)
-            print(strTweet)        
-            r = requests.post("http://text-processing.com/api/sentiment/", data={'text':strTweet })
-            print(r.status_code, r.reason)
-            print(r.json())
-            count += 1
-            up=db[collections[i]].update_one(
-            {"_id": tweetsIds[i][j] },
-            {"$set": {"label": r.json()['label'], "positive_probability": r.json()['probability']['pos'] , "negative_probability": r.json()['probability']['neg'] ,"neutral_probability": r.json()['probability']['neutral']}})
-            print(up.matched_count)
-            print(count)
-        except: #(r.status_code == 400):
-            #Empty tweet or other problem
-            count += 1
-            up=db[collections[i]].update_one(
-            {"_id": tweetsIds[i][j] },
-            {"$set": {"label": 'error', "positive_probability": 0 , "negative_probability": 0 ,"neutral_probability": 0}})
-            print(up.matched_count)
-            print(count)
-                 
+def addSentiment():
+    count = 0
+    for i,collection in enumerate(collectionsWithoutStopwords):
+        for j,tweet in enumerate(collection):
+            try:
+                strTweet = ' '.join(tweet) #Transform the list to a string        
+                r = requests.post("http://text-processing.com/api/sentiment/", data={'text':strTweet })
+                #print(r.status_code, r.reason)
+                #print(r.json())
+                count += 1
+                db[collections[i]].update_one(
+                {"_id": tweetsIds[i][j] },
+                {"$set": {"label": r.json()['label'], "positive_probability": r.json()['probability']['pos'] , "negative_probability": r.json()['probability']['neg'] ,"neutral_probability": r.json()['probability']['neutral']}})
+            except: #(r.status_code == 400 or 503):
+                #Empty tweet or other problem
+                db[collections[i]].update_one(
+                {"_id": tweetsIds[i][j] },
+                {"$set": {"label": 'error', "positive_probability"  : 0 , "negative_probability": 0 ,"neutral_probability": 0}})
+            
+def pies():
+    for collection in collections:
+        frequencies = {'pos': 0, 'neutral': 0, 'neg': 0, 'error':0 }
+        #Load all tweets from the collection
+        cursor = db[collection].find()
+        for document in cursor:
+            frequencies[document['label']]+=1
+        print(frequencies)
+        #Plot pie
+     
+#Uncomment in order to run the above
+'''  
+addSentiment()          
+pies()
+'''
